d8fa25ddf79316f75a2799c95ad1d3fe82f3f483
marcoarce23 <marcoarce23@gmail.com>
2020-08-31 13:47:15 -0400

    ttttt

--- lib/src/api/DataMap.dart
+++ lib/src/api/DataMap.dart
@@ -4,7 +4,6 @@ import 'package:virtual_match/src/model/entity/IEntity.dart';
 import 'package:virtual_match/src/model/util/Const.dart';
 import 'package:virtual_match/src/model/util/StatusCode.dart';
 
-
 Map dataMap(http.Response response) {
   Map dataMap;
 
@@ -17,22 +16,21 @@ Map dataMap(http.Response response) {
   return dataMap;
 }
 
-
 List<IEntityJson> getListIEntityJson(http.Response response, IEntityJson entity,
-      Map<String, dynamic> decodeData, List<IEntityJson> list) {
-   // final List<IEntityJson> list = new List();
+    Map<String, dynamic> decodeData, List<IEntityJson> list) {
+  // final List<IEntityJson> list = new List();
 
-    if (response.statusCode == 200) {
-      Map dataMap = json.decode(response.body);
-      List<dynamic> listDynamic = dataMap[PRIMARY_KEY];
-      //      //print(listDynamic);
-      for (int i = 0; i < listDynamic.length; i++) {
-        decodeData = listDynamic[i];
-        list.add(entity.fromJson(decodeData));
-      }
-    } else {
-      Exception('Error: Status 400');
+  if (response.statusCode == 200) {
+    Map dataMap = json.decode(response.body);
+    List<dynamic> listDynamic = dataMap[PRIMARY_KEY];
+    //      //print(listDynamic);
+    for (int i = 0; i < listDynamic.length; i++) {
+      decodeData = listDynamic[i];
+      list.add(entity.fromJson(decodeData));
     }
-
-    return list;
+  } else {
+    Exception('Error: Status 400');
   }
+  print('OOOOOOO: $list');
+  return list;
+}
