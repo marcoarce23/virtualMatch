e6b004ec1d2cec953bf481042d0ffe397b2000a9
Christian <coavchristian@hotmail.com>
2020-09-21 00:55:17 -0400

    0

---
+++ lib/src/model/entity/EntityFromJson/ListaFechasTorneo.dart
@@ -0,0 +1,23 @@
+import 'package:virtual_match/src/model/entity/IEntity.dart';
+
+class ListaFechasTorneo implements IEntityJson {
+  int id;
+  // ignore: non_constant_identifier_names
+  int nrO_FECHA;
+  String torneo;
+  DateTime fecha;
+
+  ListaFechasTorneo(
+      {this.id,
+      // ignore: non_constant_identifier_names
+      this.nrO_FECHA,
+      this.fecha,
+      this.torneo});
+
+  fromJson(Map<String, dynamic> json) => new ListaFechasTorneo(
+        id: json["id"],
+        nrO_FECHA: json["nrO_FECHA"],
+        torneo: json["torneo"],
+        fecha: json["fecha"],
+      );
+}
