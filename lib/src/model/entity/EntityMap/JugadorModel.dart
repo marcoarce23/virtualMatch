22ba7cbb07e17651fc11f1ce1a12b0f1a9f7934e
Christian <coavchristian@hotmail.com>
2020-10-17 17:33:24 -0400

    0

--- lib/src/model/entity/EntityMap/JugadorModel.dart
+++ lib/src/model/entity/EntityMap/JugadorModel.dart
@@ -72,3 +72,76 @@ class JugadorModel implements IEntityMap, IEntityJson {
         foto: json["foto"],
       );
 }
+
+class JugadorModelPersonalizado implements IEntityJson {
+  int idJugador;
+  int idOrganizacion;
+  int idaDepartamento;
+  String departamento;
+  int idLogin;
+  String idPsdn;
+  String nombre;
+  String apellido;
+  String correo;
+  String telefono;
+  int idaSexo;
+  String informacionComplementaria;
+  String facebook;
+  String twitter;
+  String foto;
+  int boot;
+
+  JugadorModelPersonalizado(
+      {this.idJugador,
+      this.idOrganizacion,
+      this.idaDepartamento,
+      this.departamento,
+      this.idLogin,
+      this.idPsdn,
+      this.nombre,
+      this.apellido,
+      this.correo,
+      this.telefono,
+      this.idaSexo,
+      this.informacionComplementaria,
+      this.facebook,
+      this.twitter,
+      this.foto,
+      this.boot});
+
+  Map<String, dynamic> toJson() => {
+        "idJugador": idJugador,
+        "idOrganizacion": idOrganizacion,
+        "idaDepartamento": idaDepartamento,
+        "departamento": departamento,
+        "idLogin": idLogin,
+        "idPsdn": idPsdn,
+        "nombre": nombre,
+        "apellido": apellido,
+        "correo": correo,
+        "telefono": telefono,
+        "idaSexo": idaSexo,
+        "informacionComplementaria": informacionComplementaria,
+        "facebook": facebook,
+        "twitter": twitter,
+        "foto": foto,
+        "boot": boot
+      };
+
+  fromJson(Map<String, dynamic> json) => new JugadorModel(
+        idJugador: json["idJugador"],
+        idOrganizacion: json["idOrganizacion"],
+        idaDepartamento: json["idaDepartamento"],
+        idLogin: json["idLogin"],
+        idaSexo: json["idaSexo"],
+        idPsdn: json["idPsdn"],
+        nombre: json["nombre"],
+        apellido: json["apellido"],
+        correo: json["correo"],
+        telefono: json["telefono"],
+        informacionComplementaria: json["informacionComplementaria"],
+        facebook: json["facebook"],
+        twitter: json["twitter"],
+        foto: json["foto"],
+      );
+}
