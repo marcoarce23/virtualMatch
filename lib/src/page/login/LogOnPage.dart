c01d316838a58e25817edca6054d45d83993f059
marcoarce23 <marcoarce23@gmail.com>
2020-11-23 19:55:48 -0400

    subir logeo

--- lib/src/page/login/LogOnPage.dart
+++ lib/src/page/login/LogOnPage.dart
@@ -182,11 +182,15 @@ class _LogOnPageState extends State<LogOnPage> {
 
     switch (result.status) {
       case AuthorizationStatus.authorized:
-        prefs.nameUser = result.credential.fullName.givenName.toString() +
-            ' ' +
-            result.credential.fullName.familyName;
+        prefs.nameUser = (result.credential.fullName == null)
+            ? 'user apple'
+            : result.credential.fullName.givenName.toString() +
+                ' user apple' +
+                result.credential.fullName.familyName;
 
-        prefs.email = result.credential.email.toString();
+        prefs.email = (result.credential.email == null)
+            ? 'email apple'
+            : result.credential.email.toString();
         prefs.avatarImage = IMAGE_LOGO;
         prefs.userId = result.credential.user.toString();
 
